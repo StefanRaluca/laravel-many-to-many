73675576eacf95c079311675186ecfeb01a70f70
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-28 15:15:57 +0200

    add show view with modal

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTechnologyRequest;
 use App\Http\Requests\UpdateTechnologyRequest;
 use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologyController extends Controller
 {
@@ -32,7 +33,14 @@ class TechnologyController extends Controller
      */
     public function store(StoreTechnologyRequest $request)
     {
-        //
+
+        $validated_data = $request->validated();
+        $slug = Str::slug($validated_data['name'], '-');
+        $validated_data['slug'] = $slug;
+        Technology::create($validated_data);
+
+        return to_route('admin.technologies.index')->with('message', "New technologies it's created!");
+
     }
 
     /**
@@ -48,7 +56,7 @@ class TechnologyController extends Controller
      */
     public function edit(Technology $technology)
     {
-        //
+        return view('admin.technologies.index', compact('technology'));
     }
 
     /**
@@ -56,7 +64,12 @@ class TechnologyController extends Controller
      */
     public function update(UpdateTechnologyRequest $request, Technology $technology)
     {
-        //
+        $validated_data = $request->validated();
+        $slug = Str::slug($validated_data['name'], '-');
+        $validated_data['slug'] = $slug;
+        $technology->update($validated_data);
+
+        return to_route('admin.technologies.index')->with('message', " $technology->name updated!");
     }
 
     /**
--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -22,7 +22,38 @@
                         <td>{{ $technology->name }}</td>
                         <td>{{ $technology->slug }}</td>
                         <td>
-                            View/Edit/Delete
+                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
+                                data-bs-target="#view-{{ $technology->id }}"><i class="fa fa-eye" aria-hidden="true"></i>
+                                View
+                            </button>
+                            <button type="button" class="btn btn-success" data-bs-toggle="modal"
+                                data-bs-target="#edit-{{ $technology->id }}"><i class="fa fa-pencil" aria-hidden="true"></i>
+                                Edit
+                            </button>
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#delete-{{ $technology->id }}"><i class="fa fa-trash"
+                                    aria-hidden="true"></i>
+                                Delete
+                            </button>
+                        </td>
+                        <div class="modal fade" id="view-{{ $technology->id }}" tabindex="-1" aria-labelledby="name"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="name">Show Technology</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h5>Name: {{ $technology->name }}</h5>
+                                        <p>ID: {{ $technology->id }}</p>
+                                        <p>Slug: {{ $technology->slug }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
