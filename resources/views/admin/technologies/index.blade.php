86ab580486eb3edd1388409d500317806714fe08
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-28 15:28:08 +0200

    add edit view with modal in index technologies

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -53,6 +53,32 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="modal fade" id="edit-{{ $technology->id }}" tabindex="-1" aria-labelledby="name"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="editModalLabel{{ $technology->id }}">Edit Technology
+                                        </h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="{{ route('admin.technologies.update', $technology) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <div class="mb-3">
+                                                <label for="name" class="form-label">Name</label>
+                                                <input type="text" class="form-control" id="name" name="name"
+                                                    value="{{ $technology->name }}">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Save Changes</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </tr>
                 @endforeach
             </tbody>
